7d9702fad6297da0593cac87627d138ac282bb9a
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 21:44:18 +0545

    Method parameters in java

---
+++ src/methods/MethodParameters.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class MethodParameters {
+    // methods/function declaration
+    static void greetUser(String name){
+        System.out.println("Hello" + name);
+    }
+    public static void main(String[] args){
+        greetUser("Milan");
+        greetUser("krishna");
+        greetUser("Sreela Prabhupad");
+
+
+
+    }
+}
