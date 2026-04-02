e7e8dcfe897b887858bab2149d8addfdf4a99d41
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 20:17:38 +0545

    For Loop

---
+++ src/ControlStatements/ForLoop.java
@@ -0,0 +1,10 @@
+package ControlStatements;
+
+public class ForLoop {
+    public static  void main(String[] args){
+        int i;
+        for (i=0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
