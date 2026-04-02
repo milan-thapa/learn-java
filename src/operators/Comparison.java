0856fa443951e206c5be486d70c5712751b5acbf
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 17:40:48 +0545

    Comparison  operators

---
+++ src/operators/Comparison.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class Comparison {
+    public static void main(String[] args) {
+
+        int x = 100;
+        int y = 50;
+
+        // Comparison operators
+        System.out.println("x > y: " + (x > y)); // true
+        System.out.println("x < y: " + (x < y)); // false
+        System.out.println("x == y: " + (x == y)); // false
+        System.out.println("x != y: " + (x != y)); // true
+        System.out.println("x >= y: " + (x >= y)); // true
+        System.out.println("x <= y: " + (x <= y)); // false
+    }
+}
