ee67b42f74de5b927c9d3d5e671106ec03b3489d
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 17:07:22 +0545

    real life example

---
+++ src/variables/RealLifeExample.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class RealLifeExample {
+    public static void main(String[] args){
+        // Student data
+        String studentName = "Milan Thapa";
+        int studentID = 12;
+        int studentAge = 23;
+        float studentFee = 7000.5f;
+        char studentGrade = 'A';
+
+        // Print variables
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
+    }
+}
