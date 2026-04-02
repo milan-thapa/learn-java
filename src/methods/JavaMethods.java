b507cb158604b2a78b0702e97b42cc4889cf90b0
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 21:33:50 +0545

    Java methods

---
+++ src/methods/JavaMethods.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class JavaMethods {
+
+    // methods/ function declaration
+
+    static  void meroMethod(){
+        System.out.println("Give me a glass of water.");
+    }
+    public static void main(String[] args){
+
+        //function call
+            meroMethod();
+    }
+}
