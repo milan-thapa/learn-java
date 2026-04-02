0223a58f1e01e020c872de092bdfcc6e38754324
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 16:43:50 +0545

    print numbers

--- src/introduction/HelloWorld.java
+++ src/introduction/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
         System.out.println("Hello, Nepal");
+        System.out.println(17);
+        System.out.println(17 + 10);
     }
 }
