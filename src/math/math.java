4e82ee2aae1f117d12402b3621a2d0e13e3370d8
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 19:14:16 +0545

    Math in java

---
+++ src/math/math.java
@@ -0,0 +1,19 @@
+package math;
+
+
+
+public class math {
+    public static void  main(String[] args){
+
+
+        System.out.println("Maximum Value between two numbers: " + Math.max(5,10));
+        System.out.println("Square of given number : " +  Math.sqrt(25));
+        System.out.println("Absulote value :" +   Math.abs(-2.7));
+        System.out.println(" Power of a given number : " +  Math.pow(2,8) );
+        System.out.println("Round Value : " +   Math.round(4.6));
+        System.out.println("Ceil Value : " +  Math.ceil(4.1));
+        System.out.println("Floor Value :" +  Math.floor(4.9));
+        System.out.println("Random Value : " +  Math.random());
+
+    }
+}
