3b87f4af364ee85fc69f94a8f5b0610a31e80e0c
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 20:56:06 +0545

    Arrays in java

---
+++ src/Arrays/JavaArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class JavaArray {
+    public static void main(String[] args){
+        String[] cars = {"Rolls Royce", "Mercedis Benz", "Ferrari",  "Bugati", "Porche","BMW", "Jaguar"};
+        System.out.println("Your Dream car : " + cars[4]);
+
+
+        String[] athletes = {"Cristiano Ronaldo", "Leonal Messi", "Nemyar JR", "Deago Maradona", "Pele"};
+        athletes[2]= "Milan Thapa";
+        System.out.println("Greatest of all time: " + athletes[2]);
+    }
+}
