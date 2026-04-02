4c3a5353bf70b0bf135478ad8105bd6dcd321ce2
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 19:21:59 +0545

    Boolen operation in java

---
+++ src/booleans/RealLifeExample.java
@@ -0,0 +1,16 @@
+package booleans;
+
+public class RealLifeExample {
+    public  static void main(String[] args){
+        int myAge = 23;
+        int votingAge = 18;
+        if(myAge >= votingAge){
+            System.out.println(" Congratulations, your are eligible to vote.");
+        }
+        else{
+            System.out.println("Sorry, You are not eligible to vote. ");
+        }
+    }
+
+
+}
