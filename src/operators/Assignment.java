4a1db4db192e0dee2fd73f64b8ceeb60214ffe27
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 17:36:15 +0545

    Assignment Operator

---
+++ src/operators/Assignment.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class Assignment {
+    public  static void main(String[] args){
+       int savings = 1000;
+       savings += 500;
+
+       System.out.println("Total Savings: " + savings);
+
+    }
+}
