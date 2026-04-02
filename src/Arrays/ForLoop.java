cb6334af41a68190411b2b4edec4ba6264be064a
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 21:03:55 +0545

    for loop in array

---
+++ src/Arrays/ForLoop.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ForLoop {
+    public static void main(String[] args){
+        String[] billioners = {"Elon Musk", "Lary Page", "Jeff Bezos ", "Mark Zuckerberg "};
+        for(int i = 0 ; i< billioners.length; i++){
+            System.out.println(billioners[i]);
+        }
+    }
+}
