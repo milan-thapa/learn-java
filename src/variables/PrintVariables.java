3b5a7a63e836e8214b3f5080b067c77fba5285a0
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 16:54:53 +0545

    print variables

---
+++ src/variables/PrintVariables.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class PrintVariables {
+    public static void main(String[] args){
+        String name = "Milan Thapa";
+        int  age = 23;
+        System.out.println("Hello, my name is "+ name + " and i'm " + age + " years old.");
+    }
+}
