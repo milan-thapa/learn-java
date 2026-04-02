019bd252833515bf8eb4a5ff76de13655d85a052
Milan Thapa <thapamilan9762@gmail.com>
2026-04-02 17:24:34 +0545

    var keyword in java

---
+++ src/DataTypes/VarKeyword.java
@@ -0,0 +1,18 @@
+package DataTypes;
+
+public class VarKeyword {
+    public static void main(String[] args){
+        var name = "Milan";
+        var age = 23;
+        var GPA = 3.33;
+        var salary =  50000;
+        var isDeveloper = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + GPA);
+        System.out.println("Salary: " + salary);
+        System.out.println("IsDeveloper " + isDeveloper);
+
+    }
+}
