b04a29347af537e5b78874d7ec3647910a96079a
Milan Thapa <thapamilan9762@gmail.com>
2026-04-03 08:55:31 +0545

    constructor in java

---
+++ src/classes/Constructor.java
@@ -0,0 +1,33 @@
+package classes;
+
+//classes
+class Car{
+    String brand;
+    String model;
+
+
+    public void CarInfo(){
+        System.out.println("Brand: " + this.brand);
+        System.out.println("Model: " + this.model);
+
+
+    }
+//non parametrized constructor
+    Car(){
+        System.out.println("constructor is calling.");
+    }
+
+}
+
+public class Constructor {
+    public  static void main(String[] args){
+
+        Car car1 = new Car();
+
+        car1.brand = "Rolls Royce";
+        car1.model = "ghost";
+
+
+        car1.CarInfo();
+    }
+}
